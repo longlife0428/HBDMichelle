cdf2054dce5b49b8f0659875433f08998b3b420a
longlife0428 <longlife0428@gmail.com>
2020-08-04 00:50:02 +0800

    完成0804

---
+++ src/settings.d745e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"f1413",resources:"04b5b",main:"7fdb9"}};
