059023689019d6cd32fedc592ea3421490e48dba
longlife0428 <longlife0428@gmail.com>
2020-08-03 20:31:17 +0800

    更新TOUCH

--- src/settings.f9b8c.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"f1413",resources:"a54f1",main:"07dfb"}};
